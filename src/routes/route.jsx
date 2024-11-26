03eed160dd49108c074ed4b36c71f3a13e958085
hammadsadi <hammad.sadi@yahoo.com>
2024-11-26 11:41:31 +0600

    Added Protected Functionality

--- src/routes/route.jsx
+++ src/routes/route.jsx
@@ -2,11 +2,16 @@ import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import Login from "../pages/Login";
 import SignUp from "../pages/SignUp";
+import PrivateRoute from "./PrivateRoute";
 
 const route = createBrowserRouter([
   {
     path: "/",
-    element: <App />,
+    element: (
+      <PrivateRoute>
+        <App />
+      </PrivateRoute>
+    ),
   },
   {
     path: "/login",
